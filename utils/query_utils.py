e8aabcfe5708a2abdcba46cf323a84ba83b221ce
arrm.1996@gmail.com <arrm.1996@gmail.com>
2020-11-14 06:59:30 -0500

    Nov 14: Mapa de descripcion

--- utils/query_utils.py
+++ utils/query_utils.py
@@ -17,47 +17,49 @@ class Sql:
 
 class Carga_query:
 
-    def __init__(self, update_map=True):
+    def __init__(self, update_map=True, agr='SUM'):
+        self.agr = agr
         self.update_map = update_map
         if self.update_map is True:
-            self.query = "SELECT ARCO, SUM(CARGA) AS CARGA FROM CONSULTA "
+            self.query = "SELECT ARCO, {}(CARGA) AS CARGA FROM CONSULTA ".format(self.agr)
         else:
-            self.query = "SELECT HORA, SUM(CARGA) AS CARGA FROM CONSULTA "
+            self.query = "SELECT HORA, {}(CARGA) AS CARGA FROM CONSULTA ".format(self.agr)
         self.number_filters = 0
 
     def add_date_range(self, start_date, end_date):
         if self.number_filters == 0:
             self.query += 'WHERE '
-            self.query += '(FECHA BETWEEN \'{}\' AND \'{}\') '.format(start_date, end_date)
+            self.query += 'FECHA BETWEEN \'{}\' AND \'{}\' '.format(start_date, end_date)
         else:
             self.query += 'AND '
-            self.query += '(FECHA BETWEEN \'{}\' AND \'{}\') '.format(start_date, end_date)
+            self.query += 'FECHA BETWEEN \'{}\' AND \'{}\' '.format(start_date, end_date)
         self.number_filters += 1
 
     def add_hour_filter(self, hour=None):
         if self.number_filters == 0 and (hour is not None and hour != []):
             self.query += 'WHERE '
-            self.query += '(HORA IN {} )'.format(str(hour).replace("[", "(").replace("]", ")"))
+            self.query += 'HORA IN {} '.format(str(hour).replace("[", "(").replace("]", ")"))
         elif hour is not None and hour != []:
             self.query += 'AND '
-            self.query += '(HORA IN {} )'.format(str(hour).replace("[", "(").replace("]", ")"))
+            self.query += 'HORA IN {} '.format(str(hour).replace("[", "(").replace("]", ")"))
         self.number_filters += 1
 
     def add_day_filter(self, dia=None):
         if self.number_filters == 0 and (dia is not None and dia != []):
             self.query += 'WHERE '
-            self.query += '(DIA IN {} )'.format(str(dia).replace("[", "(").replace("]", ")"))
+            self.query += 'DIA IN {} '.format(str(dia).replace("[", "('").replace("]", "')"))
         elif dia is not None and dia != []:
             self.query += 'AND '
-            self.query += '(DIA IN {} )'.format(str(dia).replace("[", "(").replace("]", ")"))
+            self.query += 'DIA IN {} '.format(str(dia).replace("[", "('").replace("]", "')"))
         self.number_filters += 1
 
-    def restart_query(self, update_map=True):
+    def restart_query(self, update_map=True, agr='SUM'):
+        self.agr = agr
         self.update_map = update_map
         if self.update_map is True:
-            self.query = "SELECT ARCO, SUM(CARGA) AS CARGA FROM CONSULTA "
+            self.query = "SELECT ARCO, {}(CARGA) AS CARGA FROM CONSULTA ".format(self.agr)
         else:
-            self.query = "SELECT HORA, SUM(CARGA) AS CARGA FROM CONSULTA "
+            self.query = "SELECT HORA, {}(CARGA) AS CARGA FROM CONSULTA ".format(self.agr)
         self.number_filters = 0
 
     def last_add(self):
